6fae1120fa6539ccc122b5379ff67814c15170e2
DanielBenBassat <147989550+DanielBenBassat@users.noreply.github.com>
2024-02-17 18:25:27 +0200

    f server

--- server.py
+++ server.py
@@ -6,13 +6,8 @@ FILTER = "udp"
 
 def receive_secret_message(packet):
     """
-<<<<<<< HEAD
     receive packet and if it is an empty udp packet turn the destination port to his ascii value and print it
     :param packet: packet the client send that represent an ascii value
-=======
-    receive packet and if its an empty udp packet turn the destention port to his asci value and print it
-    :param packet: packet the client send that represent an asci value
->>>>>>> 44408b0af19705e02139e96bd33bc4fab0d20604
     :return: none
     """
     if packet.haslayer(UDP) and len(packet[UDP].payload) == 0:
