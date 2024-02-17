825a412f74883980c9fd42a9e6493e3057a97052
DanielBenBassat <147989550+DanielBenBassat@users.noreply.github.com>
2024-02-17 18:21:43 +0200

    final client

--- clinet.py
+++ clinet.py
@@ -5,14 +5,14 @@ IP_ADDRESS = "192.168.68.1"
 
 def send_secret_message(message):
     """
-    send a packet for every letter in messgae, the packet is sent to the port of the asci value of the letter
+    send a packet for every letter in message, the packet is sent to the port of the ascii value of the letter
     :param message: the message that the client entered
     :return: none
     """
     for char in message:
         ascii_value = ord(char)
         packet = IP(dst=IP_ADDRESS) / UDP(dport=ascii_value)
-        send(packet)
+        send (packet)
 
 
 def main():
